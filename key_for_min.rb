ae9d26fdd44e2ce0a5b41d7b6fbe846d3759b06c
Alice Brunel <alc.brunel@gmail.com>
2019-07-09 22:34:07 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,8 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  
+  counter = []
+  name_hash.each do |value|
+    counter << value
+  end
 end
