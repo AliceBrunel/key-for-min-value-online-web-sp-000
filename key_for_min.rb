08db8bb99266da5a8d8f79576c161076cc1fabe9
Alice Brunel <alc.brunel@gmail.com>
2019-07-09 22:38:07 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,8 +2,19 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-  counter = []
-  name_hash.each do |value|
-    counter << value
+    n = array.length
+  loop do
+    swapped = false
+
+    (n-1).times do |i|
+      if array[i] > array[i+1]
+        array[i], array[i+1] = array[i+1], array[i]
+        swapped = true
+      end
+    end
+
+    break if not swapped
   end
+
+  array
 end
