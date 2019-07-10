2c2504fce610b484ccba368e7046e8de9931816e
Alice Brunel <alc.brunel@gmail.com>
2019-07-10 00:17:45 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,7 +1,7 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def key_for_min_value (name_hash)
+def key_for_min_value(name_hash)
 array = []
   name_hash.collect do |x|
   puts "#{x[1]} : #{x[0]}"
