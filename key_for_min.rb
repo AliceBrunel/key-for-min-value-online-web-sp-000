ceeabbbb8b86503a9178be62678b3cdcc22cd213
Alice Brunel <alc.brunel@gmail.com>
2019-07-10 00:16:39 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,7 +1,7 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
 
-def order_from_min_value(name_hash)
+def key_for_min_value (name_hash)
   
   name_hash.sort do |a, b|
     if a[1] == b[1]
